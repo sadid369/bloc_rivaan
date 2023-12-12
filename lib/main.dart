a7451e68201637bb1dc87e57dcc834c55c6f23cb
sadid369 <sadid.jones@gmail.com>
2023-12-12 23:07:11 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:bloc_rivaan/bloc/counter_bloc.dart';
 import 'package:bloc_rivaan/cubit/counter_cubit.dart';
 import 'package:bloc_rivaan/cubit/todo_cubit.dart';
 import 'package:bloc_rivaan/home_page.dart';
+import 'package:bloc_rivaan/login_screen.dart';
+import 'package:bloc_rivaan/pallete.dart';
 import 'package:bloc_rivaan/todo_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -33,14 +35,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => const TodoList(),
-        '/add-todo': (_) => const AddTodoPage(),
-      },
+      home: const LoginScreen(),
     );
   }
 }
